ba61fe8784cdef21cbaa14819dda3a10f5c751e6
Roy Schor <rschor@justworks.com>
2025-10-27 19:54:35 -0400

    redploying

--- save-the-date/src/components/CardFront.jsx
+++ save-the-date/src/components/CardFront.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import TornImage from "./TornImage";
 import "./CardFront.css";
 
+const cardSrc = new URL("/card.jpg", import.meta.env.BASE_URL).href;
+
 export default function CardFront() {
   return (
     <div className="card-front-container">
-      <TornImage src="/card.jpg" alt="Save the date" />
+      <TornImage src={cardSrc} alt="Save the date" />
       <div className="card-front-content">
         <p className="card-front-subtitle">Save the Date</p>
         <p className="card-front-date">August 10, 2026</p>
