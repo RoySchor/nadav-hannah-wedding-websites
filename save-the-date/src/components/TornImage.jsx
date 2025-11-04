fc5dd8d0f3b3af141d6f01d1d4976657e14f0e43
Roy Schor <rschor@justworks.com>
2025-11-03 20:01:09 -0500

    removing unnecessary image css

--- save-the-date/src/components/TornImage.jsx
+++ save-the-date/src/components/TornImage.jsx
@@ -5,13 +5,6 @@ export default function TornImage({ src, alt }) {
   return (
     <div className="torn-image-container">
       <img src={src} alt={alt} className="torn-image" draggable={false} />
-      {/* Torn edge */}
-      <svg className="torn-edge" viewBox="0 0 100 20" preserveAspectRatio="none" aria-hidden>
-        <path
-          d="M0 10 Q 5 0, 10 10 T 30 10 T 50 10 T 70 10 T 90 10 T 110 10 V20 H0 Z"
-          className="torn-edge-fill"
-        />
-      </svg>
     </div>
   );
 }
