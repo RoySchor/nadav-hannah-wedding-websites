5ba713f76178f18f63a53cfbb356ef03b463ff9a
Roy Schor <rschor@justworks.com>
2025-11-06 21:11:32 -0500

    updating wording, favicon, and form

--- save-the-date/src/components/CardFront.jsx
+++ save-the-date/src/components/CardFront.jsx
@@ -7,9 +7,9 @@ export default function CardFront() {
     <div className="card-front-container">
       <TornImage src="/card.jpg" alt="Save the date" />
       <div className="card-front-content">
-        <p className="card-front-subtitle">Save the Date</p>
-        <p className="card-front-date">August 10, 2026</p>
-        <p className="card-front-location">"Somewhere in the PA woods"</p>
+        <p className="card-front-subtitle">We are tying the knot</p>
+        <p className="card-front-date">August 29, 2026</p>
+        <p className="card-front-location">Poconos, PA</p>
         <p className="card-front-instruction">Tap the card to share your details</p>
       </div>
     </div>
