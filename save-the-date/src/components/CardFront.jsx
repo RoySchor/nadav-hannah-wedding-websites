2fb1802df28b3a679031245b56c85b0d3ed5dd88
Roy Schor <rschor@justworks.com>
2026-06-18 11:18:01 -0400

    style: separate festivities text to new line below date

--- save-the-date/src/components/CardFront.jsx
+++ save-the-date/src/components/CardFront.jsx
@@ -8,7 +8,8 @@ export default function CardFront() {
       <TornImage src="/card.jpg" alt="Save the date" />
       <div className="card-front-content">
         <p className="card-front-subtitle">We are tying the knot</p>
-        <p className="card-front-date">June 11-13th, 2027</p>
+        <p className="card-front-date">June 12th, 2027</p>
+        <p className="card-front-festivities">with festivities all weekend</p>
         <p className="card-front-location">Camp Lindenmere, Poconos, PA</p>
         <p className="card-front-instruction">Tap the card to share your details</p>
       </div>
